d80e64ebf80a017e045a4f8a0d74f2f90723a9b3
Sameer Siddiqui <ssiddi28@gmail.com>
2015-11-17 17:26:14 -0600

    Initial Commit

---
+++ Dua/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dua
+//
+//  Created by Sameer Siddiqui on 11/17/15.
+//  Copyright © 2015 Sameer Siddiqui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Dua/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dua
+//
+//  Created by Sameer Siddiqui on 11/17/15.
+//  Copyright © 2015 Sameer Siddiqui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
